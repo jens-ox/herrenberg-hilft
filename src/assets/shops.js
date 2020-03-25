76be8215a10f7c1452caa0f8d329e4f06325b212
Jens Ochsenmeier <github@jens-ox.de>
2020-03-25 15:29:53 +0100

    squoosh images

--- src/assets/shops.js
+++ src/assets/shops.js
@@ -10,6 +10,6 @@ module.exports = [{
   description: 'Wir bieten unsere Getränke und Speisen täglich von 11 bis 20 Uhr zur Abholung oder ab einem Mindestbestellwert von 25€ bis 10km zur Lieferung frei Haus an und eine Rolle Toilettenpapier gratis dazu.',
   phone: '07032/914923',
   url: 'https://www.barista-cafe.com/',
-  image: 'assets/img/barista.png',
+  image: 'assets/img/barista.jpg',
   tags: ['cafe', 'getränke', 'trinken', 'liefern']
 }]
